87ac2129aa558250eb1fff485dfe107097918ed9
uoot_ubuntu <cyduoot@hotmail.com>
2017-10-15 17:16:16 +0800

    forget to write readseg

--- boot/bootmain.c
+++ boot/bootmain.c
@@ -24,9 +24,13 @@
 #include <aim/boot.h>
 #include <elf.h>
 
+#define SECTSIZE 512
+
 const uint8_t magic[] = {
 		0x7f, 0x45, 0x4c, 0x46
 	};
+
+void readseg(void*, uint, uint);
 
 __noreturn
 void bootmain(void)
@@ -34,22 +38,22 @@ void bootmain(void)
 	struct elf_hdr *elf;
 	struct elf_phdr *ph, *eph;
 	void (*entry)(void);
-	uchar *pa;
+	void *pa;
 
 	elf = (struct elf_hdr*)0x10000;
 
-	readseg((uchar*)elf, 4096, 0);
+	readseg((void*)elf, 4096, 0);
 
 	for (int i = 0; i < sizeof(magic); i++) {
 		if (elf->e_ != magic[i]) goto bad;
 	} // check elf magic number
 
 //load segment
-	ph = (struct elf_phdr*)((uchar*)elf + elf->phoff);
+	ph = (struct elf_phdr*)((void*)elf + elf->phoff);
 	eph = ph + elf->phnum;
 	for (; ph < eph; ph++)
 	{
-		pa = (uchar*)ph->paddr;
+		pa = (void*)ph->paddr;
 		readseg(pa, ph->filesz, ph->offset);
 	}
 
@@ -58,4 +62,35 @@ void bootmain(void)
 
 bad:
 	while (1);
+}
+
+void waitdisk(void)
+{
+	while ((inb(0x1F7) & 0xC0) != 0x40) ;
+}
+
+void readsect(void *dst, uint offset)
+{
+	waitdisk();
+	outb(0x1F2, 1);
+    outb(0x1F3, offset);
+    outb(0x1F4, offset >> 8);
+    outb(0x1F5, offset >> 16);
+    outb(0x1F6, (offset >> 24) | 0xE0);
+    outb(0x1F7, 0x20);
+
+    waitdisk();
+    insl(0x1F0, dst, SECTSIZE/4);
+}
+
+void readseg(void* pa, uint count, uint offset)
+{
+	void *epa;
+	epa = pa + count;
+	pa −= offset % SECTSIZE;
+
+    offset = (offset/SECTSIZE) + 1;  
+
+    for(; pa < epa; pa += SECTSIZE, offset++)
+        readsect(pa, offset);
 }
